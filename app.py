1c7791054b66a346e7d53745a762c29aac3c546f
areph <40balmung@gmail.com>
2021-07-09 13:12:44 +0900

    download file

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 import asyncio
+import urllib.request
 from pyppeteer import launch
 
 async def main():
+    # WSL2で実行しているのでheadless前提
     browser = await launch(args=['--no-sandbox'])
+
+    print("== Start ==")
+
     page = await browser.newPage()
-    await page.goto('https://example.com')
-    element = await page.querySelector('h1')
-    title = await page.evaluate('(element) => element.textContent', element)
-    await page.screenshot({'path': 'example.png'})
-    print("====")
-    print(title)
-    print("====")
+    # 気象庁の気象データを取得する
+    await page.goto('https://www.data.jma.go.jp/obd/stats/data/mdrr/docs/csv_dl_format_prenh.html')
+    element = await page.querySelector('#main>ul>li>a')
+
+    ## 取得したaタグのURLを取りに行く
+    link = await page.evaluate('(element) => element.href', element)
+
+    # --download--
+    # puppeteerがfile downloadに対応しておらず、リクエストが中途半端に切れてしまう
+    # await page._client.send('Page.setDownloadBehavior', {'behavior': 'allow', 'downloadPath': './'});
+    # await page.goto(link)
+
+    # そのためリンクを取得したらurllibを使って別でダウンロードする
+    urllib.request.urlretrieve(link, './最新の気象データ.csv')
+
+    print("== End ==")
     await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())
